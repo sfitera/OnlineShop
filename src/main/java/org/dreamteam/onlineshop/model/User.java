63a9c86adb90886db3dcd7ad95e9267b5d01de85
zuzanaknutova <Zuzana.knut@gmail.com>
2025-01-25 18:32:57 +0100

    Merge pull request #29 from sfitera/6-create-orderitem-class
    6 create orderitem class

--- src/main/java/org/dreamteam/onlineshop/model/User.java
+++ src/main/java/org/dreamteam/onlineshop/model/User.java
@@ -1,4 +1,29 @@
 package org.dreamteam.onlineshop.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.dreamteam.onlineshop.model.enums.UserRole;
+
+@Builder
+@Data
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    private Long id;
+    private User user;
+    private String userName;
+    private String userPassword;
+    private String userAddress;
+    private String userEmail;
+    private UserRole userRole;
+
+
 }

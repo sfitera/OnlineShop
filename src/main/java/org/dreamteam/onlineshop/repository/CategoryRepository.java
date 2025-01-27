6868e2e8b1d6a178dfd07c3f30318d8cdc0ce5ea
freepiti <freepiti@seznam.cz>
2025-01-27 18:23:41 +0100

    delete import cartegory

--- src/main/java/org/dreamteam/onlineshop/repository/CategoryRepository.java
+++ src/main/java/org/dreamteam/onlineshop/repository/CategoryRepository.java
@@ -1,6 +1,5 @@
 package org.dreamteam.onlineshop.repository;
 
-import jdk.jfr.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

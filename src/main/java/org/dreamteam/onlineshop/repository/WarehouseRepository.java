73032a20368f87fad4298c01868751e79adb5e5a
zuzanaknutova <Zuzana.knut@gmail.com>
2025-01-27 17:50:13 +0100

    Update Product User

---
+++ src/main/java/org/dreamteam/onlineshop/repository/WarehouseRepository.java
@@ -0,0 +1,6 @@
+package org.dreamteam.onlineshop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
+}

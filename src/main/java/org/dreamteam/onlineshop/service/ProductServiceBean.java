f4bdbd6c2ce1a3dca5cda4f2b4ff2f37d5bac33c
zuzanaknutova <Zuzana.knut@gmail.com>
2025-02-03 18:18:16 +0100

    #44 ProductServiceBean creation, Product update

---
+++ src/main/java/org/dreamteam/onlineshop/service/ProductServiceBean.java
@@ -0,0 +1,123 @@
+package org.dreamteam.onlineshop.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.dreamteam.onlineshop.model.Category;
+import org.dreamteam.onlineshop.model.Product;
+
+
+import org.dreamteam.onlineshop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+@Slf4j
+@Service
+
+public class ProductServiceBean implements  ProductService {
+
+    int amount = 0;
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceBean(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    @Override
+    public Product addProduct(String name, Double price, int quantity, Boolean availability, String image, Category category, String author, String description) {
+        Product product = new Product(name, price, quantity, availability, image, category, author, description);
+        productRepository.save(product);
+        return product;
+    }
+
+
+    @Override
+    public Product updateProduct(Long id, Product updateProduct) {
+        var product = productRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Product with id " + id + " does not exist"));
+        var updated = new Product();
+        if (StringUtils.hasText(updateProduct.getProductName())) {
+            updated.setProductName(updateProduct.getProductName());
+        } else {
+            updated.setProductName(product.getProductName());
+        }
+
+        if (updateProduct.getProductPrice() != 0) updated.setProductPrice(product.getProductPrice());
+        else {
+            updated.setProductPrice(product.getProductPrice());
+        }
+
+        if (updateProduct.getProductQuantity() != 0)
+            updated.setProductQuantity(product.getProductQuantity());
+        else {
+            updated.setProductQuantity(product.getProductQuantity());
+        }
+
+
+        if (updateProduct.getProductCategory() != null) {
+            updated.setProductCategory(updateProduct.getProductCategory());
+        } else {
+            updated.setProductCategory(product.getProductCategory());
+        }
+
+        if (StringUtils.hasText(updateProduct.getProductAuthor())) {
+            updated.setProductAuthor(updateProduct.getProductAuthor());
+        } else {
+            updated.setProductAuthor(product.getProductAuthor());
+        }
+
+        if (StringUtils.hasText(updateProduct.getProductDescription())) {
+            updated.setProductDescription(updateProduct.getProductDescription());
+        } else {
+            updated.setProductDescription(product.getProductDescription());
+        }
+
+        productRepository.save(updated);
+        log.info("Product updated: {}", updated);
+        return updated;
+
+    }
+
+    @Override
+    public Product getProduct(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        if (!productRepository.existsById(id)) {
+            throw new IllegalArgumentException("Product with id" + id + "does not exist");
+        }
+        productRepository.deleteById(id);
+    }
+
+
+    @Override
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByAuthor(String author) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndAuthor(String category, String author) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+}

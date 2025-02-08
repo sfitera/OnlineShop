7c416df57523185901aa60b070ab305993f6119f
unknown <fitera.stefan@gmail.com>
2025-02-08 16:26:55 +0100

    102: change to data.sql, images and app.properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,146 +1,134 @@
 INSERT INTO products (product_name, product_price, product_quantity, product_availability, product_image,
                       product_category, product_author, product_description)
-VALUES ("Syn", 15.99, 30, TRUE, "static/images/Syn.jpeg", "DETECTIVE", "Jo Nesbo",
+VALUES ("Syn", 15.99, 30, TRUE, "images/Syn.jpeg", "DETECTIVE", "Jo Nesbo",
         "Autor sa venuje korupcii a zároveň rieši nevypovedané veci medzi otcom a synom"),
 
-       ("Hry o život. Skúška ohňom.", 20.49, 0, FALSE, "static/images/Hry o zivot.jpeg", "FANTASY","Suzanne Collinsová",
+       ("Hry o život. Skúška ohňom", 20.49, 0, FALSE, "images/Hry o zivot.jpeg", "FANTASY","Suzanne Collinsová",
         "Fantazijný príbeh z katastrofickej budúcnosti ,v ktorom sa deti bijú o svoj život v hre v, ktorej môže vyhrať len jeden"),
 
-       ("Pútnik z Compostely. Mágov denník.", 17.99, 20, TRUE, "static/images/Putnik z Compostely.jpeg", "ROMANCE","Paolo Coelho",
+       ("Pútnik z Compostely. Mágov denník", 17.99, 20, TRUE, "images/Putnik z Compostely.jpeg", "ROMANCE","Paolo Coelho",
         "Putovanie do svojho vnútra prostredníctvom pútavého príbehu odvážneho cestovateľa"),
 
-       ("Harry Potter a tajomná komnata", 9.99, 20, TRUE, "static/images/Harry Potter a tajomna komnata.jpeg","FANTASY",
+       ("Harry Potter a tajomná komnata", 9.99, 20, TRUE, "images/Harry Potter a tajomna komnata.jpeg","FANTASY",
         "J.K.Rowlingová", "Druhý príbeh Harryho Pottera"),
 
-       ("Harry Potter a kameň mudrcov", 19.99, 15, TRUE, "static/images/Harry Potter a kamen mudrcov.jpeg", "FANTASY",
+       ("Harry Potter a kameň mudrcov", 19.99, 15, TRUE, "images/Harry Potter a kamen mudrcov.jpeg", "FANTASY",
         "J.K.Rowlingová", "Príbeh Harryho Pottera  čarodejníckej siroty s obrovskou odvahou a kúzelnými schopnosťami"),
 
-       ("Páví kráľ", 21.99, 7, TRUE, "static/images/Pavi kral.jpeg", "CHILDREN", "D ' Aulnoy", "Francúzske rozprávky"),
+       ("Páví kráľ", 21.99, 7, TRUE, "images/Pavi kral.jpeg", "CHILDREN", "D ' Aulnoy", "Francúzske rozprávky"),
 
-       ("Zlatý Skarabeus", 9.77, 7, TRUE, "static/images/Zlaty scarabeus.jpeg", "HORROR", "Edgar Allan Poe",
+       ("Zlatý Skarabeus", 9.77, 7, TRUE, "images/Zlaty scarabeus.jpeg", "HORROR", "Edgar Allan Poe",
         "Hororová poviedka"),
 
-       ("Povedz iba slovo ", 5.50, 0, FALSE, "static/images/Povedz iba slovo.jpeg", "POETRY", "Pavel Hirax Baričák",
+       ("Povedz iba slovo", 5.50, 0, FALSE, "images/Povedz iba slovo.jpeg", "POETRY", "Pavel Hirax Baričák",
         "Slovenská ľúbostná poézia"),
 
-       ("Veronika sa rozhodla zomrieť.", 13.99, 20, TRUE, "static/images/Veronika sa rozhodla zomriet.jpeg", "ROMANCE",
+       ("Veronika sa rozhodla zomrieť", 13.99, 20, TRUE, "images/Veronika sa rozhodla zomriet.jpeg", "ROMANCE",
         "Paolo Coelho", "Príbeh prostitútky"),
 
-       ("Kráľovská kobra z Káthmandu", 12.59, 4, TRUE, "static/images/Kralovska kobra z Kathamandu.jpeg", "CHILDREN",
+       ("Kráľovská kobra z Káthmandu", 12.59, 4, TRUE, "images/Kralovska kobra z Kathamandu.jpeg", "CHILDREN",
         "P.B.KERR", "Príbeh mladých džinov"),
 
-       ("Matilda", 23.99, 6, TRUE, "static/images/Matilda.jpeg", "CHILDREN", "Roald Dahl",
+       ("Matilda", 23.99, 6, TRUE, "images/Matilda.jpeg", "CHILDREN", "Roald Dahl",
         "Príbeh nadaného dievčatko vyrastajúceho v rodine kde sa nestretla s podporou a pochopením"),
 
-       ("Harry Potter a polovičný princ.", 12.99, 23, TRUE, "static/images/Harry Potter a polovicny princ.jpeg", "FANTASY",
+       ("Harry Potter a polovičný princ", 12.99, 23, TRUE, "images/Harry Potter a polovicny princ.jpeg", "FANTASY",
         "J.K.Rowlingová", "Šiesty príbeh Harryho Pottera"),
 
-       ("Temné hmoty", 25.99, 23, TRUE, "static/images/Temne hmoty.jpeg", "FANTASY", "Philip Pullman",
+       ("Temné hmoty", 25.99, 23, TRUE, "images/Temne hmoty.jpeg", "FANTASY", "Philip Pullman",
         "Trilogia o svete kde temne hmoty ovládajú svet"),
 
-       ("Dezerty a cukrovinky", 18.99, 0, FALSE, "static/images/Dezerty a cukrovinky.jpeg", "COOKING", "Mary Berryová",
+       ("Dezerty a cukrovinky", 18.99, 0, FALSE, "images/Dezerty a cukrovinky.jpeg", "COOKING", "Mary Berryová",
         "Zlatá kniha cukroviniek z kuchyne Mary Berryovej"),
 
-       ("Harry Potter a fénixov rád.", 43.99, 23, TRUE, "static/images/Harry Potter a fenixov rad.jpeg", "FANTASY",
+       ("Harry Potter a fénixov rád", 43.99, 23, TRUE, "images/Harry Potter a fenixov rad.jpeg", "FANTASY",
         "J.K.Rowlingová", "Piaty príbeh Harryho Pottera"),
 
-       ("Vesmír", 99.99, 10, TRUE, "static/images/Vesmir.jpeg", "EDUCATION", "Skupina autorov",
+       ("Vesmír", 99.99, 10, TRUE, "images/Vesmir.jpeg", "EDUCATION", "Skupina autorov",
         "Veľká obrazová encyklopédia vesmíru"),
 
-       ("Harry Potter a väzeň z Askabanu.", 43.99, 23, TRUE, "static/images/Harry Potter a vezen z askabanu.jpeg", "FANTASY",
+       ("Harry Potter a väzeň z Askabanu", 43.99, 23, TRUE, "images/Harry Potter a vezen z askabanu.jpeg", "FANTASY",
         "J.K.Rowlingová", "Tretí príbeh Harryho Pottera"),
 
-       ("Slovenské rozprávky", 25.99, 12, TRUE, "static/images/Slovenske rozpravky.jpeg", "CHILDREN", "Pavol Dobšinský",
+       ("Slovenské rozprávky", 25.99, 12, TRUE, "images/Slovenske rozpravky.jpeg", "CHILDREN", "Pavol Dobšinský",
         "Slovenské rozprávky z našej klenotnice"),
 
-       ("Výživa podľa piatich elementov", 9.99, 3, TRUE, "static/images/Vyziva podla piatich elementov.jpeg", "EDUCATION",
+       ("Výživa podľa piatich elementov", 9.99, 3, TRUE, "images/Vyziva podla piatich elementov.jpeg", "EDUCATION",
         "Barbara Temehe", "Výživový poradca podľa elementov"),
 
-       ("Metro 2033", 25.99, 23, TRUE, "static/images/Metro 2033.jpeg", "FANTASY", "Dmitry Glukhovsky",
+       ("Metro 2033", 25.99, 23, TRUE, "images/Metro 2033.jpeg", "FANTASY", "Dmitry Glukhovsky",
         "Svet po apokalypse odohrávajúci sa  pod zemou v útrobách metra"),
 
-       ("Modry džin babylonsky.", 12.59, 4, TRUE, "static/images/Modry dzin babylonsky.jpeg", "CHILDREN", "P.B.KERR",
-        "Príbeh mladých džinov"),
-
-       ("Zlatý kľúčik", 25.99, 12, TRUE, "static/images/Zlaty klucik.jpeg", "CHILDREN", "Alexander Tolstoj",
-        "Ruska kopia Pinokiových dobrodružstiev"),
-
-       ("Zostup do Maelstromu", 66.77, 0, FALSE, "static/images/Zostup do Maelstromu.jpeg", "HORROR", "Edgar Allan Poe",
+       ("Zostup do Maelstromu", 66.77, 0, FALSE, "images/Zostup do Maelstromu.jpeg", "HORROR", "Edgar Allan Poe",
         "Hororová poviedka"),
 
-       ("Ako rieka ktorá plynie...", 17.99, 20, TRUE, "static/images/Ako rieka ktora plynie.jpeg", "ROMANCE", "Paolo Coelho",
+       ("Ako rieka ktorá plynie...", 17.99, 20, TRUE, "images/Ako rieka ktora plynie.jpeg", "ROMANCE", "Paolo Coelho",
         "Úvahy a zamyslenia"),
 
-       ("Strach múdreho muža ", 25.99, 23, TRUE, "static/images/Strach mudreho muza .jpeg", "FANTASY", "Patrick Rothfuss",
+       ("Strach múdreho muža", 25.99, 23, TRUE, "images/Strach mudreho muza.jpeg", "FANTASY", "Patrick Rothfuss",
         "Druhy diel príbehu o čarodejníckom učňovi"),
 
-       ("Deň džinovských bojovníkov.", 16.59, 77, TRUE, "static/images/Den dzinovskych bojovnikov.jpeg", "CHILDREN",
+       ("Deň džinovských bojovníkov", 16.59, 77, TRUE, "images/Den dzinovskych bojovnikov.jpeg", "CHILDREN",
         "P.B.KERR", "Príbeh mladých džinov"),
 
-       ("Pôvod", 15.99, 3, TRUE, "static/images/Povod.jpeg", "DETECTIVE", "Dan Brown",
+       ("Pôvod", 15.99, 3, TRUE, "images/Povod.jpeg", "DETECTIVE", "Dan Brown",
         "Detektívny  príbeh obalený cestopisom a históriou miest v ktorých sa odohráva"),
 
-       ("Rastlina", 9.99, 0, FALSE, "static/images/Rastlina.jpeg", "EDUCATION", "Janet Marinelli",
+       ("Rastlina", 9.99, 0, FALSE, "images/Rastlina.jpeg", "EDUCATION", "Janet Marinelli",
         "Veľká obrazová encyklopédia rastlín celého sveta"),
 
-       ("Brida ", 11.99, 20, TRUE, "static/images/Brida.jpeg", "ROMANCE", "Paolo Coelho", "Román"),
+       ("Brida ", 11.99, 20, TRUE, "images/Brida.jpeg", "ROMANCE", "Paolo Coelho", "Román"),
 
-       ("Alfonz Vrtinôžka.", 77.59, 0, FALSE, "static/images/Alfonz Vrtinozka.jpeg", "CHILDREN", "Gerhard Holtz Baumer",
+       ("Alfonz Vrtinôžka", 77.59, 0, FALSE, "images/Alfonz Vrtinozka.jpeg", "CHILDREN", "Gerhard Holtz Baumer",
         "Vtipne príbehy chlapca vyrastajúceho v DDR"),
 
-       ("More srdca", 5.50, 4, TRUE, "static/images/More srdca.jpeg", "POETRY", "Pavel Hirax Baričák", "Slovenská poézia"),
+       ("More srdca", 5.50, 4, TRUE, "images/More srdca.jpeg", "POETRY", "Pavel Hirax Baričák", "Slovenská poézia"),
 
-       ("Porozprávam ti ...", 2.99, 1, TRUE, "static/images/Porozpravam ti .jpeg", "EDUCATION", "Jorge Bucay",
+       ("Porozprávam ti ...", 2.99, 1, TRUE, "images/Porozpravam ti.jpeg", "EDUCATION", "Jorge Bucay",
         "Príbehy ľudí v kritických životných situáciách"),
 
-       ("Človek", 99.99, 10, TRUE, "static/images/Clovek.jpeg", "EDUCATION", "Skupina autorov",
+       ("Človek", 99.99, 10, TRUE, "images/Clovek.jpeg", "EDUCATION", "Skupina autorov",
         "Veľká obrazová encyklopédia človeka"),
 
-       ("Kamos obor", 23.99, 6, TRUE, "static/images/Kamos obor.jpeg", "CHILDREN", "Roald Dahl",
+       ("Kamos obor", 23.99, 6, TRUE, "images/Kamos obor.jpeg", "CHILDREN", "Roald Dahl",
         "Príbeh o obrovi ktorý pomáhal ľuďom"),
 
-       ("Harry Potter a dary smrti.", 73.99, 23, TRUE, "static/images/Harry Potter a dary smrti.jpeg", "FANTASY",
+       ("Harry Potter a dary smrti", 73.99, 23, TRUE, "images/Harry Potter a dary smrti.jpeg", "FANTASY",
         "J.K.Rowlingová", "Siedmi príbeh Harryho Pottera"),
 
-       ("Pyramídy Stráže!", 73.99, 23, TRUE, "static/images/Pyramidy Straze.jpeg", "FANTASY", "Terry Pratchet",
+       ("Pyramídy Stráže!", 73.99, 23, TRUE, "images/Pyramidy Straze.jpeg", "FANTASY", "Terry Pratchet",
         "Príbehy ze zemeplochy"),
 
-       ("K asebe", 5.50, 4, TRUE, "static/images/K sebe.jpeg", "POETRY", "Pavel Hirax Baričák", "Slovenská poézia"),
+       ("K sebe", 5.50, 4, TRUE, "images/K sebe.jpeg", "POETRY", "Pavel Hirax Baričák", "Slovenská poézia"),
 
-       ("Odvaha k ľudskosti", 33.50, 4, TRUE, "static/images/Odvaha k ludskosti.jpeg", "BIOGRAPHY", "Peter Bárdy",
+       ("Odvaha k ľudskosti", 33.50, 4, TRUE, "images/Odvaha k ludskosti.jpeg", "BIOGRAPHY", "Peter Bárdy",
         "Odvaha k ľudskosti Zuzana Čaputová skutočný príbeh"),
 
-       ("Emma", 19.99, 20, TRUE, "static/images/Emma.jpeg", "ROMANCE", "Jane Austen",
+       ("Emma", 19.99, 20, TRUE, "images/Emma.jpeg", "ROMANCE", "Jane Austen",
         "Roman o Emme ktorá sa snažila ovplyvňovať vzťahy ľudí okolo seba"),
 
-       ("Farbiar a krásna Zuzana", 13.99, 6, TRUE, "static/images/Farbiar a krasna Zuzana.jpeg", "CHILDREN", "Ján Štiavnický",
+       ("Farbiar a krásna Zuzana", 13.99, 6, TRUE, "images/Farbiar a krasna Zuzana.jpeg", "CHILDREN", "Ján Štiavnický",
         "Rozprávky"),
 
-       ("Malá morská panna", 133.99, 0, FALSE, "static/images/Mala morska panna.jpeg", "CHILDREN", "H.CH.Andersen",
+       ("Malá morská panna", 133.99, 0, FALSE, "images/Mala morska panna.jpeg", "CHILDREN", "H.CH.Andersen",
         "Rozprávky slávneho spisovateľa"),
 
-       ("Stratený symbol", 17.99, 0, FALSE, "static/images/Strateny symbol.jpeg", "DETECTIVE", "Dan Brown",
+       ("Stratený symbol", 17.99, 0, FALSE, "images/Strateny symbol.jpeg", "DETECTIVE", "Dan Brown",
         "Detektívny  príbeh obalený cestopisom a históriou miest v ktorých sa odohráva"),
 
-       ("Oceán", 99.99, 10, TRUE, "static/images/Ocean.jpeg", "EDUCATION", "Skupina autorov",
+       ("Oceán", 99.99, 10, TRUE, "images/Ocean.jpeg", "EDUCATION", "Skupina autorov",
         "Odkrývanie tajimstiev poslednej divočiny sveta"),
 
-       ("Magicky prazdroj. Soudne sestry ", 53.99, 23, TRUE, "static/images/Magicky prazdroj.jpeg", "FANTASY",
+       ("Magicky prazdroj. Soudne sestry ", 53.99, 23, TRUE, "images/Magicky prazdroj.jpeg", "FANTASY",
         "Terry Pratchet", "Príbehy ze zemeplochy "),
 
-       ("Výživa podľa piatich elementov", 9.99, 3, TRUE, "static/images/Vyziva podla piatich elementov.jpeg", "EDUCATION",
-        "Barbara Temehe", "Výživový poradca podľa elementov"),
-
-       ("Metro 2033", 25.99, 23, TRUE, "static/images/Metro 2033.jpeg", "FANTASY", "Dmitry Glukhovsky",
-        "Svet po apokalypse odohrávajúci sa  pod zemou v útrobách metra"),
-
-       ("Modry džin babylonsky.", 12.59, 4, TRUE, "static/images/Modry dzin babylonsky.jpeg", "CHILDREN", "P.B.KERR",
+       ("Modry džin babylonsky", 12.59, 4, TRUE, "images/Modry dzin babylonsky.jpeg", "CHILDREN", "P.B.KERR",
         "Príbeh mladých džinov"),
 
-       ("Zlatý kľúčik", 25.99, 12, TRUE, "static/images/Zlaty klucik.jpeg", "CHILDREN", "Alexander Tolstoj",
+       ("Zlatý kľúčik", 25.99, 12, TRUE, "images/Zlaty klucik.jpeg", "CHILDREN", "Alexander Tolstoj",
         "Ruska kopia Pinokiových dobrodružstiev"),
 
-       ("Cierny kocur", 77.77, 0, FALSE, "static/images/Cierny kocur.jpeg", "HORROR", "Edgar Allan Poe", "Hororová poviedka"),
+       ("Cierny kocur", 77.77, 0, FALSE, "images/Cierny kocur.jpeg", "HORROR", "Edgar Allan Poe", "Hororová poviedka"),
 
-       ("Harry Potter a ohniva casa.", 73.99, 23, TRUE, "static/images/Harry Potter a ohniva casa.jpeg", "FANTASY",
+       ("Harry Potter a ohniva casa", 73.99, 23, TRUE, "images/Harry Potter a ohniva casa.jpeg", "FANTASY",
         "J.K.Rowlingová", "Štvrtý príbeh Harryho Pottera");
